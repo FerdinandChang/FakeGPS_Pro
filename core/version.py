60f3ab0f05b7bf73ce91ec8a747c9f812f34ff89
chiaming <oferdinando@msn.com>
2026-09-06 15:38:35 +0800

    chore: Prepare release v1.1.0 and enable tag-based Mac release upload

--- core/version.py
+++ core/version.py
@@ -1,7 +1,7 @@
 """
 FakeGPS Pro - Application Version Definition
 """
-CURRENT_VERSION = "1.0.0"
+CURRENT_VERSION = "1.1.0"
 
 def parse_version(ver_str: str):
     """解析版本字串為數字元組，例如 'v1.2.3' -> (1, 2, 3)"""
